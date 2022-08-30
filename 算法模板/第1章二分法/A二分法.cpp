df9e70f6c20ea4e466cbdb87c0937177f0c6a046
zzhuncle <57178872+zzhuncle@users.noreply.github.com>
2022-08-30 13:19:50 +0800

    Update A二分法.cpp

--- 算法模板/第1章二分法/A二分法.cpp
+++ 算法模板/第1章二分法/A二分法.cpp
@@ -1,7 +1,11 @@
 //LeetCode 35.搜索插入位置
 //1. 区间的选择：[l, r) 左开右闭
 //2. 区间缩小的边界 l = mid + 1, r = mid
-//3. 返回值：返回时 l == r, 
+//3. 返回值：返回时 l == r 求下界时直接返回 l, 即 >= 的下界或者 > 的下界
+
+
+// upper_bound 查找[first, last)区域中第一个大于 val 的元素
+// lower_bound 查找[first, last)区域中第一个不小于 val 的元素
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
